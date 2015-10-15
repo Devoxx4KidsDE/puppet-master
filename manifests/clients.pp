c9eb7e0dc030a62a574059af2f0b6ec0f1983709
Tobias Schneider <schneider@synyx.de>
2015-10-15 12:57:34 +0200

    add nodes 13-16

--- manifests/clients.pp
+++ manifests/clients.pp
@@ -1,4 +1,4 @@
-node d4k-1,d4k-2, d4k-3, d4k-4 {
+node d4k-1, d4k-2, d4k-3, d4k-4 {
   class { 'client': } ->
   class { 'maze_vr': }
 }
@@ -11,4 +11,8 @@ node d4k-5, d4k-6, d4k-7, d4k-8 {
 node d4k-9, d4k-10, d4k-11, d4k-12 {
   class { 'client': } ->
   class { 'ws_jumping_sumo': }
+}
+
+node d4k-13, d4k-14, d4k-15, d4k-16 {
+  class { 'client': }
 }
