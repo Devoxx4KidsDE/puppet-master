acdceafc61e8e6a7932660cee9982656792bca9e
Tobias Schneider <schneider@synyx.de>
2015-10-15 10:31:56 +0200

    separate node definition to workshops

--- manifests/clients.pp
+++ manifests/clients.pp
@@ -1,6 +1,14 @@
-node /^d4k-\d+$/ {
+node d4k-1,d4k-2, d4k-3, d4k-4 {
   class { 'client': } ->
-  class { 'maze_vr': } ->
-  class { 'ws_minecraft': } ->
+  class { 'maze_vr': }
+}
+
+node d4k-4, d4k-5, d4k-6, d4k-7 {
+  class { 'client': } ->
+  class { 'ws_minecraft': }
+}
+
+node d4k-8, d4k-9, d4k-10, d4k-11 {
+  class { 'client': } ->
   class { 'ws_jumping_sumo': }
 }
