da82949a92739910c3d723791b519da04e960677
Tobias Schneider <schneider@synyx.de>
2015-10-15 10:39:38 +0200

    correct numbers of pis

--- manifests/clients.pp
+++ manifests/clients.pp
@@ -3,12 +3,12 @@ node d4k-1,d4k-2, d4k-3, d4k-4 {
   class { 'maze_vr': }
 }
 
-node d4k-4, d4k-5, d4k-6, d4k-7 {
+node d4k-5, d4k-6, d4k-7, d4k-8 {
   class { 'client': } ->
   class { 'ws_minecraft': }
 }
 
-node d4k-8, d4k-9, d4k-10, d4k-11 {
+node d4k-9, d4k-10, d4k-11, d4k-12 {
   class { 'client': } ->
   class { 'ws_jumping_sumo': }
 }
