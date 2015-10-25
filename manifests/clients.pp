5130fb677963e6414540772e56ba1495cc69b880
Tobias Schneider <schneider@synyx.de>
2015-10-25 13:03:49 +0100

    change module names

--- manifests/clients.pp
+++ manifests/clients.pp
@@ -1,19 +1,19 @@
 node d4k-1, d4k-2, d4k-3, d4k-4 {
   class { 'd4k::client': } ->
-  class { 'd4k::workshop::maze_vr': }
+  class { 'd4k::workshop_maze_vr': }
 }
 
 node d4k-5, d4k-6, d4k-7, d4k-8 {
   class { 'd4k::client': } ->
-  class { 'd4k::workshop::minecraft': }
+  class { 'd4k::workshop_minecraft': }
 }
 
 node d4k-9, d4k-10, d4k-11, d4k-12 {
   class { 'd4k::client': } ->
-  class { 'd4k::workshop::jumping_sumo': }
+  class { 'd4k::workshop_jumping_sumo': }
 }
 
 node d4k-13, d4k-14, d4k-15, d4k-16 {
   class { 'd4k::client': } ->
-  class { 'd4k::workshop::tinkerforge': }
+  class { 'd4k::workshop_tinkerforge_electricity': }
 }
