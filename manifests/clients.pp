32b374c09496118feb80eaf7c91a9cb753a786a5
Tobias Schneider <schneider@synyx.de>
2015-11-26 08:57:59 +0100

    add 13 and 15 to minecraft and maze

--- manifests/clients.pp
+++ manifests/clients.pp
@@ -13,7 +13,19 @@ node d4k-9, d4k-10, d4k-11, d4k-12 {
   class { 'd4k::workshop_jumping_sumo': }
 }
 
-node d4k-13, d4k-14, d4k-15, d4k-16 {
+node d4k-14, d4k-16 {
   class { 'd4k::client': } ->
   class { 'd4k::workshop_tinkerforge_electricity': }
+}
+
+node d4k-13 {
+  class { 'd4k::client': } ->
+  class { 'd4k::workshop_tinkerforge_electricity': } ->
+  class { 'd4k::workshop_maze_vr': }
+}
+
+node d4k-15 {
+  class { 'd4k::client': } ->
+  class { 'd4k::workshop_tinkerforge_electricity': } ->
+  class { 'd4k::workshop_minecraft_collect_game': }
 }
