8c6962aa25f420faaf1e58119d9d052d3fd1e33d
Tobias Schneider <schneider@synyx.de>
2016-04-25 14:02:07 +0200

    all nodes get the all workshops

--- manifests/clients.pp
+++ manifests/clients.pp
@@ -1,38 +1,7 @@
-node d4k-1, d4k-2, d4k-3, d4k-4 {
+node d4k-1, d4k-2, d4k-3, d4k-4, d4k-5, d4k-6, d4k-7, d4k-8, d4k-9, d4k-10, d4k-11, d4k-12, d4k-13, d4k-14, d4k-15, d4k-16 {
   class { 'd4k::client': } ->
-  class { 'd4k::workshop_maze_vr': }
-}
-
-node d4k-5, d4k-6, d4k-7, d4k-8 {
-  class { 'd4k::client': } ->
-  class { 'd4k::workshop_minecraft_collect_game': }
-}
-
-node d4k-9, d4k-10, d4k-11, d4k-12 {
-  class { 'd4k::client': } ->
-  class { 'd4k::workshop_jumping_sumo': }
-}
-
-node d4k-13 {
-  class { 'd4k::client': } ->
+  class { 'd4k::workshop_jumping_sumo': } ->
   class { 'd4k::workshop_tinkerforge_electricity': } ->
   class { 'd4k::workshop_maze_vr': } ->
-  class { 'd4k::workshop_jumping_sumo': }
-}
-
-node d4k-14 {
-  class { 'd4k::client': } ->
-  class { 'd4k::workshop_tinkerforge_electricity': }->
-  class { 'd4k::workshop_jumping_sumo': }
-}
-
-node d4k-16 {
-  class { 'd4k::client': } ->
-  class { 'd4k::workshop_tinkerforge_electricity': }
-}
-
-node d4k-15 {
-  class { 'd4k::client': } ->
-  class { 'd4k::workshop_tinkerforge_electricity': } ->
-  class { 'd4k::workshop_minecraft_collect_game': }
+  class { 'd4k::workshop_minecraft_raspberry': }
 }
