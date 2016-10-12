cb21b83fb4b635be5aafde5fabccd59cf160bcb4
Tobias Schneider <schneider@synyx.de>
2016-10-12 13:39:18 +0200

    add Jumping sumo 4 scratch workshop

--- manifests/clients.pp
+++ manifests/clients.pp
@@ -3,5 +3,6 @@ node d4k-1, d4k-2, d4k-3, d4k-4, d4k-5, d4k-6, d4k-7, d4k-8, d4k-9, d4k-10, d4k-
   class { 'd4k::workshop_jumping_sumo': } ->
   class { 'd4k::workshop_tinkerforge_electricity': } ->
   class { 'd4k::workshop_maze_vr': } ->
-  class { 'd4k::workshop_minecraft_raspberry': }
+  class { 'd4k::workshop_minecraft_raspberry': } ->
+  class { 'd4k::workshop_jumping_sumo_4_scratch': }
 }
